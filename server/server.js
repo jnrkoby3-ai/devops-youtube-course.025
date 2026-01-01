c9eb9a4c13f4e8dcf6a734c3093250aa378da47f
jnrkoby3-ai <jnrkoby3@gmail.com>
2025-12-31 17:06:04 -0800

    dev

--- server/server.js
+++ server/server.js
@@ -53,7 +53,7 @@ mongoose
 
 // Graceful shutdown
 process.on('SIGINT', async () => {
-  console.log('Magnificiently - Shutting down gracefully...');
+  console.log('dev - Shutting down gracefully...');
   await mongoose.connection.close();
   process.exit(0);
 });
